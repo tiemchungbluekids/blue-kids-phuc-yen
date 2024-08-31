222da0ff9c1641d79cd6cfcf1d7bbf7f7279e53e
tiemchungbluekids <info@tiemchungbluekids.vn>
2024-08-31 09:26:25 +0700

    fix

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -9,7 +9,7 @@ const Banner: React.FC<BannerProps> = ({
   slogan = "Vì một tương lai khỏe mạnh cho con bạn",
   buttonText = "Đặt lịch tiêm ngay",
   buttonLink = "/dat-lich",
-  imageUrl = "/images/banner.jpg"
+  imageUrl = "/images/banner-moi.jpg"
 }) => {
   return (
     <div className="relative h-[500px]">
@@ -22,9 +22,9 @@ const Banner: React.FC<BannerProps> = ({
       <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center">
         <h1 className="text-4xl font-bold text-white mb-4">{title}</h1>
         <p className="text-xl text-white mb-8">{slogan}</p>
-        <Link href={buttonLink} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+        {/* <Link href={buttonLink} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
           {buttonText}
-        </Link>
+        </Link> */}
       </div>
     </div>
   );
